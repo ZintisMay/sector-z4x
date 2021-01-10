6c8117155405a8710b7ccd12457c988f061b15ba
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2021-01-10 18:32:10 -0500

    youtube

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-console.log("bob");
+window.addEventListener("click", function (e) {
+  console.log(e)
+  window.target = e.target
+  if (e.target.id == "menu") toggleMenu();
+  if (e.target.tagName == "A") toggleMenu();
+  if (e.target.classList.contains("bgImage")) openLightBox(e);
+  if (["lightBox", "closeLightBox"].includes(e.target.id)) closeLightBox();
+});
 
-menu.addEventListener('click', function(){
-  menu.classList.toggle("show")   
-})
+function toggleMenu() {
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
+    menu.classList.add("unShow");
+  } else {
+    menu.classList.add("show");
+    menu.classList.remove("unShow");
+  }
+}
+
+function openLightBox(e) {
+  let image = e.target.style.backgroundImage.replace(`url("`,"").replace(`")`,"")
+  console.log(image);
+  lightBox.classList.add("show");
+  lightBoxImage.src = image;
+}
+
+function closeLightBox() {
+  lightBox.classList.remove("show");
+}
+
+
