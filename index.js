e81eddc47eaafb1922a85bbc3dd2d2fbe7fbde3a
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2021-01-11 20:15:41 -0500

    bgrounds

--- index.js
+++ index.js
@@ -1,53 +1,58 @@
 window.addEventListener("click", function (e) {
-  log(e);
-  window.target = e.target;
-  if (e.target.id == "menu") toggleMenu();
-  if (e.target.tagName == "A") closeMenu();
-  if (e.target.classList.contains("bgImage")) openLightBox(e);
-  if (["lightBox", "closeLightBox"].includes(e.target.id)) closeLightBox();
-  if (e.target.id == "openSignup") openPlaytestSignup();
+  log(e)
+  window.target = e.target
+  if (e.target.id == "menu") toggleMenu()
+  if (e.target.tagName == "A") closeMenu()
+  if (e.target.classList.contains("bgImage")) openLightBox(e)
+  if (["lightBox", "closeLightBox"].includes(e.target.id)) closeLightBox()
+  if (e.target.id == "openSignup") openPlaytestSignup()
 });
 
 function log(...args) {
   if (window.location.href.includes("127.0.0.1")) {
-    console.log(...args);
+    console.log(...args)
   }
+  return args[0]
 }
 
 function toggleMenu() {
-  log("toggleMenu");
+  log("toggleMenu")
   if (menu.classList.contains("show")) {
-    menu.classList.remove("show");
-    menu.classList.add("unShow");
+    menu.classList.remove("show")
+    menu.classList.add("unShow")
   } else {
-    menu.classList.add("show");
-    menu.classList.remove("unShow");
+    menu.classList.add("show")
+    menu.classList.remove("unShow")
   }
 }
 
 function closeMenu() {
-  log("closeMenu");
-  menu.classList.remove("show");
-  menu.classList.add("unShow");
+  log("closeMenu")
+  menu.classList.remove("show")
+  menu.classList.add("unShow")
 }
 
 function openLightBox(e) {
-  log("openLightBox");
+  log("openLightBox")
   let image = e.target.style.backgroundImage
     .replace(`url("`, "")
-    .replace(`")`, "");
-  log(image);
-  lightBox.classList.add("show");
-  lightBoxImage.src = image;
+    .replace(`")`, "")
+  log(image)
+  lightBox.classList.add("show")
+  lightBoxImage.src = image
 }
 
 function closeLightBox() {
-  log("closeLightBox");
-  lightBox.classList.remove("show");
+  log("closeLightBox")
+  lightBox.classList.remove("show")
 }
 
 function openPlaytestSignup() {
-  log("openPlaytestSignup");
-  let html = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSepksc_J3nV8gVws4z7ygCzyyWdTB2zMZJJlZqII5L_b6AUnQ/viewform?embedded=true" width="640" height="852" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`;
-  signup.innerHTML = html;
+  log("openPlaytestSignup")
+  let width = log(window.innerWidth)
+  let height = log(window.innerHeight)
+  if(width > 600) width = 600
+  if(height > 800) height = 800
+  let html = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSepksc_J3nV8gVws4z7ygCzyyWdTB2zMZJJlZqII5L_b6AUnQ/viewform?embedded=true" width="${.9 * width}" height="${.9 * height}" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`
+  signup.innerHTML = html
 }
